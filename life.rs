778f09236b7e13beef3352bb00a486863fb794b8
unknown <badamson@SEA-1201014970.ant.amazon.com>
2013-12-26 11:52:55 -0800

    Worked some more today.
    I think I am at the point with the grid/cell data structures that they
    should be usable to implement the games rules.

--- life.rs
+++ life.rs
@@ -1,11 +1,15 @@
 static GRID_SIZE: uint = 10;
+static GRID_MAX_INDEX: uint = GRID_SIZE - 1;
 
+#[deriving(Eq)]
 enum CellValue {
   alive, dead
 }
 
 struct Cell {
-  cv: CellValue
+  cv: CellValue,
+  row: Row,
+  column: Column
 }  // struct Cell
 
 impl Cell {
@@ -13,6 +17,7 @@ impl Cell {
     This fn prints to std::io indicating whether the cell is alive or dead.
   */
   fn print(&self) {
+    //print(fmt!("%u", *self.row));
     match self.cv {
       alive => print("O"),
       dead  => print("X") 
@@ -21,8 +26,8 @@ impl Cell {
   /*
     Standard ctor, returns an 'alive' cell.
   */
-  fn new() -> Cell {
-    Cell { cv: alive } 
+  fn new(row_param: Row, col_param: Column) -> Cell {
+    Cell { cv: alive, row: row_param, column: col_param } 
   }
   
   /*
@@ -33,14 +38,26 @@ impl Cell {
   }  // fn flip
 }  // impl Cell
 
-struct Height(uint);
-struct Width(uint);
+struct Row(uint);
+struct Column(uint);
 
 struct Grid {
   priv inner: [ [Cell, .. GRID_SIZE], ..GRID_SIZE],
 }
 
 impl Grid {
+
+  /*
+    Invokes an arbitrary fn on each cell.
+  */
+  fn each_cell(&self, function: &fn()) {
+    let grid = &self.inner;
+    for outer in grid.iter() {
+      for _ in outer.iter() {
+        function();  
+      }
+    }
+  }  // fn each
   /*
     Invokes print on each cell, separating rows with newlines.
   */
@@ -55,45 +72,136 @@ impl Grid {
   }  // fn print
 
   /*
-    This function returns a GRID_SIZE by GRID_SIZE 2D array (GRID) 
+    This function returns a GRID_MAX_INDEX by GRID_MAX_INDEX 2D array (GRID) 
     initially all set to dead.
   */
   fn new() -> Grid {
-    Grid { inner: [[ Cell::new(), .. GRID_SIZE], .. GRID_SIZE] }
+    // TODO: Is this a good way to indent ??
+    let mut result = Grid {
+        inner: [[ Cell::new(Row(0), Column(0)), .. GRID_SIZE], .. GRID_SIZE]
+    };
+
+    // TODO: Do I need explicit for loops? Rather use an algorithm ..
+    for o in range(0, GRID_MAX_INDEX) {
+      for i in range(0, GRID_MAX_INDEX) {
+        let cell = Cell::new(Row(i), Column(o));
+        result.inner[o][i] = cell; 
+      }  // 'for i' loop
+    } // 'for o' loop
+    result
   }  // fn new
   /*
     This fn flips the value from dead => alive, or visa versa.
   */
-  fn flip_cell(& mut self, w: Width, h: Height) {
-    println(fmt!("w %u h %u", *w, *h));
-    assert!(*w < GRID_SIZE && *h < GRID_SIZE);
-    assert!(*h < GRID_SIZE);
+  fn flip_cell(& mut self, col: Column, row: Row) {
+    println(fmt!("w %u h %u", *col, *row));
+    assert!(*col < GRID_MAX_INDEX && *row < GRID_MAX_INDEX);
+    assert!(*row < GRID_MAX_INDEX);
     // Delegate how to flip to the cell itself.
-    self.inner[*w][*h].flip();
-  }
+    self.inner[*col][*row].flip();
+  }  // fn flip_cell
 
   /*
     Returns a reference to the cell just to the right
-    of the cell with a width 'w'.
+    of the cell with a column 'w'.
+    NOTE: The 'r  is a lifetime identifier. Here, 'r is telling me that the returned Cell
+    borrowed pointer is the same lifetime as the 'self' parameter.
   */
-  fn get_right_neighbor(&self, w: Width, h: Height) -> &Cell {
-    // this SHOULD return a ref to itself, no?
-    &self.inner[0][0]
-  }  // fn get_right_neighbor
+  fn get_right_neighbor<'r>(&'r self, col: Column, row: Row) -> &'r Cell {
+    match *col {
+      GRID_MAX_INDEX => &self.inner[*row][0],  // Wrap around back to left side.
+      _         => &self.inner[*row][*col + 1]
+    }
+  }  // fn get_right_neighbor 
+
+  fn get_left_neighbor<'r>(&'r self, col: Column, row: Row) -> &'r Cell {
+    match *col {
+      0 => &self.inner[*row][GRID_MAX_INDEX], // Wrap around to right side.
+      _ => &self.inner[*row][*col - 1]
+    }
+  }  // fn get_left_neighbor
+
+  fn get_above_neighbor<'r>(&'r self, col: Column, row: Row) -> &'r Cell {
+    match *row {
+      0 => &self.inner[GRID_MAX_INDEX][*col], // Wrap around to bottom side.
+      _ => &self.inner[*row - 1][*col]
+    }
+  }  // fn get_above_neighbor
+
+  fn get_below_neighbor<'r>(&'r self, col: Column, row: Row) -> &'r Cell {
+    match *row {
+      GRID_MAX_INDEX => &self.inner[0][*col], // Wrap around to top side.
+      _         => &self.inner[*row + 1][*col]
+    }
+  } // fn get_below_neighbor
+
+  fn get_upper_left_neighbor<'r>(&'r self, col: Column, row: Row) -> &'r Cell {
+    let left_neighbor = self.get_left_neighbor(col, row);
+    // retun the above neighbor of the left_neighbor
+    return self.get_above_neighbor(left_neighbor.column, left_neighbor.row);
+  }  // fn get_upper_left_neighbor
+
+  fn get_upper_right_neighbor<'r>(&'r self, col: Column, row: Row) -> &'r Cell {
+    let right_neighbor = self.get_right_neighbor(col, row);
+    return self.get_above_neighbor(right_neighbor.column, right_neighbor.row);
+  }  // fn get_upper_right_neighbor
+
+  fn get_below_right_neighbor<'r>(&'r self, col: Column, row: Row) -> &'r Cell {
+    let right_neighbor = self.get_right_neighbor(col, row);
+    return self.get_below_neighbor(right_neighbor.column, right_neighbor.row);
+  }  // fn get_below_right_neighbor
+
+  fn get_below_left_neighbor<'r>(&'r self, col: Column, row: Row) -> &'r Cell {
+    let left_neighbor = self.get_left_neighbor(col, row);
+    return self.get_below_neighbor(left_neighbor.column, left_neighbor.row);
+  }  // fn get_below_left_neighbor
 
   /*
     This fn returns the number of dead neighbors for a cell within the grid.
   */
-  fn count_dead_neighbors(&self, w: Width, h: Height) -> uint {
-    let above_neighbor: &Cell = self.get_right_neighbor(w, h);
-    0
+  fn count_dead_neighbors(&self, col: Column, row: Row) -> uint {
+    let check_cell = |cell: &Cell| -> uint {
+      match cell.cv {
+        alive => 1,
+        dead => 0
+      }
+    };
+
+    let arr = [
+        check_cell(self.get_left_neighbor(col, row)),
+        check_cell(self.get_right_neighbor(col, row)),
+        check_cell(self.get_above_neighbor(col, row)),
+        check_cell(self.get_below_neighbor(col, row)),
+        check_cell(self.get_upper_left_neighbor(col, row)),
+        check_cell(self.get_upper_right_neighbor(col, row)),
+        check_cell(self.get_below_right_neighbor(col, row)),
+        check_cell(self.get_below_left_neighbor(col, row)),
+    ];
+    let accumulator = |v: &[uint]| -> uint {
+      let mut aggregate = 0u;
+      for item in v.iter() {
+        aggregate += *item;
+      }
+      aggregate
+    };
+
+    return accumulator(arr);
   }  // fn count_dead_neighbors
 }
 
 fn main () {
-  println("hello");
   let mut gr: Grid = Grid::new();
-  gr.print();
-  gr.flip_cell(Width(0), Height(0));
-  gr.print();
+  gr.flip_cell(Column(0), Row(0));
+
+  for outer in gr.inner.iter() {
+    for &inner in outer.iter() {
+      let dead_neighbor_count = gr.count_dead_neighbors(Column(0), Row(0));
+      if (dead_neighbor_count < 2) {
+        inner.flip();
+      }
+    }
+  }
+
+  let dead_count = gr.count_dead_neighbors(Column(0), Row(0));
+  print(fmt!("%u", dead_count));
 }
