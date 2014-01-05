061fab3e3ba8b16591d29aaaea45d1eb52a81425
Benjamin Adamson <adamson.benjamin@gmail.com>
2014-01-05 11:24:40 -0800

    Working on figuring out how to read/write files.

--- main.rs
+++ main.rs
@@ -1,31 +1,30 @@
 use std::io::Timer;
 use grid_builder::build_from_file_contents;
 use grid_builder::print_neighbor_count;
+
+use std::io::{File, result};
+use std::path::Path;
+use std::os;
 mod grid_builder;
 mod grid;
 
 fn main() {
-/*
-  let fake: ~[~str] = ~[
-    ~"......",
-    ~".oo...",
-    ~".oo...",
-    ~"...oo.",
-    ~"...oo.",
-    ~"......"
-  ];
-*/
-  //let fake = ~[ ~"....", ~".oo.", ~".oo.", ~"...." ];
-  /*let fake = ~[
-    ~"......",
-    ~"..oo..",
-    ~".o..o.",
-    ~"..oo..",
-    ~"......",
-    ~"......"
-  ];
-*/
-
+  let args = os::args();
+  assert_eq!(2, args.len());
+  let mut iter = args.iter().skip(1); // skip program name
+  
+  // todo: figure out how to not have this loop!
+  for filename in iter {
+    println!("Opening file {:s}", filename.as_slice());
+    let path = &Path::new(filename.as_slice());
+    let file_reader = match File::open(path) {
+      Some(file) => Ok(file),
+      None       => Err("err opening file.")
+    };
+    let file_contents = file_reader.unwrap().read_to_end();
+    let grid_from_file = grid_builder::build_from_file_contents(file_contents);
+    break;
+  }
   let fake = ~[ ~".....", ~".....", ~".ooo.", ~".....", ~".....", ~"....." ];
   let grid_from_file = grid_builder::build_from_file_contents(fake);
   grid_from_file.print();
